31f834a5b0eb78eec87db8003e5d37dcc1e42972
Brigette Doelp <brigette.doelp@gmail.com>
2020-07-24 16:55:48 -0700

    Add querySelectors

---
+++ src/main.js
@@ -0,0 +1,35 @@
+var hamburgerButton = document.querySelector(".hamburger-button");
+var showStarredButton = document.querySelector(".show-starred");
+var saveIdeaButton = document.querySelector(".save-idea-button");
+var searchIdeaButton = document.querySelector(".search-idea-button");
+var ideaStar = document.querySelector(".box-star");
+var ideaDelete = document.querySelector(".box-x");
+var titleInput = document.querySelector(".title-input");
+var bodyInput = document.querySelector(".body-input");
+var searchInput = document.querySelector(".search-idea-input");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//adding functionality to the save button,
+//and having the completed idea card appear
+//in its designated area.
+
+//first! enable button functionality by linking
+//the js up with the HTML.
+//
+.
